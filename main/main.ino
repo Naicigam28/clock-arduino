4248af3c6a0097a7fae79cd4b0df5f7a0b02b28f
Keanu Dirkse <k.h.dirkse78@gmail.com>
2022-12-21 15:26:08 +0200

    adding setup

---
+++ main/main.ino
@@ -0,0 +1,20 @@
+#include <LiquidCrystal.h>
+
+const int pin_RS = 8; 
+const int pin_EN = 9; 
+const int pin_d4 = 4; 
+const int pin_d5 = 5; 
+const int pin_d6 = 6; 
+const int pin_d7 = 7; 
+const int pin_BL = 10; 
+
+LiquidCrystal lcd( pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+  lcd.print("Booting Up");
+  Serial.begin(9600);
+  lcd.clear();
+
+}
